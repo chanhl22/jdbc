e4731dc5b952054cb368db325e4af29406d89f28
chanhl22 <lch9502@naver.com>
2025-02-05 14:56:30 +0900

    feat: 이체 예외처리

--- src/test/java/hello/service/MemberServiceV1Test.java
+++ src/test/java/hello/service/MemberServiceV1Test.java
@@ -15,7 +15,6 @@ import static hello.jdbc.connection.ConnectionConst.URL;
 import static hello.jdbc.connection.ConnectionConst.USERNAME;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * 기본 동작, 트랜잭션이 없어서 문제 발생
@@ -36,6 +35,13 @@ class MemberServiceV1Test {
         memberService = new MemberServiceV1(memberRepository);
     }
 
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
     @Test
     @DisplayName("정상 이체")
     void accountTransfer() throws SQLException {
@@ -53,6 +59,28 @@ class MemberServiceV1Test {
         Member findMemberB = memberRepository.findById(memberB.getMemberId());
         assertThat(findMemberA.getMoney()).isEqualTo(8000);
         assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체중 예외 발생")
+    void accountTransferEx() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        //when
+        assertThatThrownBy(() ->
+                memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        //then
+        Member findMemberA = memberRepository.findById(memberA.getMemberId());
+        Member findMemberEx = memberRepository.findById(memberEx.getMemberId());
+        //memberA의 돈만 2000원 줄었고, ex의 돈은 10000원 그대로이다.
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberEx.getMoney()).isEqualTo(10000);
     }
 
 }
